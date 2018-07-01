075585139d826aa126b035274f5d3eae1f56551e
liuziangexit <liuziang@liuziangexit.com>
2018-07-02 04:05:03 +0800

    微小的修改；

--- src/EmbedICCProfile.cpp
+++ src/EmbedICCProfile.cpp
@@ -20,13 +20,13 @@ static const std::string jfif_identify = { 0x4A, 0x46, 0x49, 0x46, 0x00 };
 static const std::string icc_profile_identify = { 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4f, 0x46, 0x49, 0x4C, 0x45, 0x00, 0x01, 0x01 };
 static const int soi_and_app0_size_without_thumbnail = soi.size() + app0_marker.size() + 16;
 static constexpr int segment_length_size = 2;
-static const bool is_little_endian_value = [] {
+static const bool is_little_endian = [] {
 	const unsigned int i = 0x11223344;
 	return *reinterpret_cast<const unsigned char*>(&i) == 0x44;
 }();
 
 inline short to_int_16(const unsigned char *tail, int index) {
-	if (is_little_endian_value)
+	if (is_little_endian)
 		return static_cast<short>(static_cast<unsigned short>(tail[index + 1] << 8) + static_cast<unsigned char>(tail[index]));
 	else
 		return static_cast<short>(static_cast<unsigned short>(tail[index] << 8) + static_cast<unsigned char>(tail[index + 1]));
@@ -76,7 +76,7 @@ bool EmbedICCProfile(unsigned char * const image, int32_t image_size, int32_t im
 		*output_size = image_size;
 		return false;
 	}
-		
+
 	std::unique_ptr<unsigned char[]> app2_segment_size_buffer(new unsigned char[segment_length_size]);
 	std::unique_ptr<unsigned char[]> app0_header_size_pun(new unsigned char[segment_length_size]);
 	std::unique_ptr<unsigned char[]> app0(new unsigned char[soi_and_app0_size_without_thumbnail]);
@@ -85,13 +85,13 @@ bool EmbedICCProfile(unsigned char * const image, int32_t image_size, int32_t im
 	unsigned char *ptr = app0.get() + soi.size() + app0_marker.size();
 	for (std::size_t i = 0; i < segment_length_size; i++, ptr++)
 		app0_header_size_pun[i] = *ptr;
-	if (is_little_endian_value)
+	if (is_little_endian)
 		std::reverse(app0_header_size_pun.get(), app0_header_size_pun.get() + segment_length_size);
 
 	int32_t	app0_header_size = to_int_16(app0_header_size_pun.get(), 0);
 	int32_t app2_segment_size = icc_profile_size + icc_profile_identify.size() + segment_length_size;
 
-	if (is_little_endian_value) {
+	if (is_little_endian) {
 		app2_segment_size_buffer[0] = *(reinterpret_cast<unsigned char *>(&app2_segment_size) + 1);
 		app2_segment_size_buffer[1] = *(reinterpret_cast<unsigned char *>(&app2_segment_size));
 	}
@@ -139,7 +139,7 @@ int main(int argc, char **argv) {
 
 		std::unique_ptr<unsigned char[]>  jpg(new unsigned char[reserve_size]);
 		std::unique_ptr<unsigned char[]> icc(new unsigned char[icc_size]);
-		
+
 		auto fp = get_file_ptr(fopen(argv[1], "rb"));
 		if (!fp) {
 			std::cout << "Can not open " + std::string(argv[1]) << "\n";
@@ -160,14 +160,13 @@ int main(int argc, char **argv) {
 			return 0;
 		}
 
-		//close file
 		fp.reset(0);
-		
+
 		int outsize = 0;
 		if (!EmbedICCProfile(jpg.get(), jpg_size, reserve_size, icc.get(), icc_size, &outsize)) {
 			std::cout << "EmbedICCProfile failed\n";
 			return 0;
-		}		
+		}
 		if (outsize == 0) {
 			std::cout << "EmbedICCProfile failed\n";
 			return 0;
