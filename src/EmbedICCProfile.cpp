deb79f122382400ad64ba23d21a87e00eb8446c5
liuziangexit <liuziang@liuziangexit.com>
2018-07-02 16:11:27 +0800

    静态!

--- src/EmbedICCProfile.cpp
+++ src/EmbedICCProfile.cpp
@@ -11,25 +11,26 @@
 
 #include <cstdio>
 #include <cstring>
+#include <cstdint>
 
-static const std::string soi{ char(0xFF), char(0xD8) };
-static const std::string app0_marker = { char(0xFF), char(0xE0) };
-static const std::string app1_marker = { char(0xFF), char(0xE1) };
-static const std::string app2_marker = { char(0xFF), char(0xE2) };
-static const std::string jfif_identify = { 0x4A, 0x46, 0x49, 0x46, 0x00 };
-static const std::string icc_profile_identify = { 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4f, 0x46, 0x49, 0x4C, 0x45, 0x00, 0x01, 0x01 };
-static const int soi_and_app0_size_without_thumbnail = soi.size() + app0_marker.size() + 16;
+static constexpr unsigned char soi[]{ 0xff,0xd8 };
+static constexpr unsigned char app0_marker[]{ 0xff,0xe0 };
+static constexpr unsigned char app1_marker[]{ 0xff,0xe1 };
+static constexpr unsigned char app2_marker[]{ 0xff,0xe2 };
+static constexpr unsigned char jfif_identify[]{ 0x4a, 0x46, 0x49, 0x46, 0x00 };
+static constexpr unsigned char icc_profile_identify[]{ 0x49, 0x43, 0x43, 0x5f, 0x50, 0x52, 0x4f, 0x46, 0x49, 0x4c, 0x45, 0x00, 0x01, 0x01 };
+static constexpr int soi_and_app0_size_without_thumbnail = sizeof(soi) + sizeof(app0_marker) + 16;
 static constexpr int segment_length_size = 2;
 static const bool is_little_endian = [] {
 	const unsigned int i = 0x11223344;
 	return *reinterpret_cast<const unsigned char*>(&i) == 0x44;
 }();
 
-inline short to_int_16(const unsigned char *tail, int index) {
+inline int16_t to_int_16(const unsigned char *tail, int index) {
 	if (is_little_endian)
-		return static_cast<short>(static_cast<unsigned short>(tail[index + 1] << 8) + static_cast<unsigned char>(tail[index]));
+		return static_cast<int16_t>(static_cast<uint16_t>(tail[index + 1] << 8) + static_cast<unsigned char>(tail[index]));
 	else
-		return static_cast<short>(static_cast<unsigned short>(tail[index] << 8) + static_cast<unsigned char>(tail[index + 1]));
+		return static_cast<int16_t>(static_cast<uint16_t>(tail[index] << 8) + static_cast<unsigned char>(tail[index + 1]));
 }
 
 using file_ptr = std::unique_ptr<FILE, void(*)(FILE *)>;
@@ -58,7 +59,7 @@ std::size_t file_size(const std::string& filename) {
 
 //计算图像置入色彩文件后的大小
 inline uint32_t get_expected_file_size(uint32_t image_size, uint32_t icc_profile_size) {
-	return image_size + app2_marker.size() + segment_length_size + icc_profile_identify.size() + icc_profile_size;
+	return image_size + sizeof(app2_marker) + segment_length_size + sizeof(icc_profile_identify) + icc_profile_size;
 }
 
 //置入色彩文件到图像
@@ -70,48 +71,48 @@ bool EmbedICCProfile(unsigned char * const image, int32_t image_size, int32_t im
 		return false;
 	}
 
-	uint32_t expected_file_size = image_size + app2_marker.size() + segment_length_size + icc_profile_identify.size() + icc_profile_size;
+	std::size_t expected_file_size = image_size + sizeof(app2_marker) + segment_length_size + sizeof(icc_profile_identify) + icc_profile_size;
 
-	if (static_cast<uint32_t>(image_reserve_size) < expected_file_size) {
+	if (static_cast<std::size_t>(image_reserve_size) < expected_file_size) {
 		*output_size = image_size;
 		return false;
 	}
 
-	std::unique_ptr<unsigned char[]> app2_segment_size_buffer(new unsigned char[segment_length_size]);
-	std::unique_ptr<unsigned char[]> app0_header_size_pun(new unsigned char[segment_length_size]);
-	std::unique_ptr<unsigned char[]> app0(new unsigned char[soi_and_app0_size_without_thumbnail]);
-	memcpy(app0.get(), image, soi_and_app0_size_without_thumbnail);
+	unsigned char app2_segment_size_buffer[segment_length_size];
+	unsigned char app0_header_size_pun[segment_length_size];
+	unsigned char app0[soi_and_app0_size_without_thumbnail];
+	memcpy(app0, image, soi_and_app0_size_without_thumbnail);
 
-	unsigned char *ptr = app0.get() + soi.size() + app0_marker.size();
+	unsigned char *ptr = app0 + sizeof(soi) + sizeof(app0_marker);
 	for (std::size_t i = 0; i < segment_length_size; i++, ptr++)
 		app0_header_size_pun[i] = *ptr;
 	if (is_little_endian)
-		std::reverse(app0_header_size_pun.get(), app0_header_size_pun.get() + segment_length_size);
+		std::reverse(app0_header_size_pun, app0_header_size_pun + segment_length_size);
 
-	int32_t	app0_header_size = to_int_16(app0_header_size_pun.get(), 0);
-	int32_t app2_segment_size = icc_profile_size + icc_profile_identify.size() + segment_length_size;
+	int16_t	app0_header_size = to_int_16(app0_header_size_pun, 0);
+	uint32_t app2_segment_size = icc_profile_size + sizeof(icc_profile_identify) + segment_length_size;
 
 	if (is_little_endian) {
 		app2_segment_size_buffer[0] = *(reinterpret_cast<unsigned char *>(&app2_segment_size) + 1);
 		app2_segment_size_buffer[1] = *(reinterpret_cast<unsigned char *>(&app2_segment_size));
 	}
 	else {
-		memcpy(app2_segment_size_buffer.get(), reinterpret_cast<unsigned char *>(&app2_segment_size), 2);
+		memcpy(app2_segment_size_buffer, reinterpret_cast<unsigned char *>(&app2_segment_size), 2);
 	}
 
 	std::unique_ptr<unsigned char[]> jpg_image_without_header(new unsigned char[image_size]);
 	memcpy(jpg_image_without_header.get(), image, image_size);
 
-	unsigned char *ptr2 = image + soi.size() + app0_marker.size() + app0_header_size;
-	memcpy(ptr2, app2_marker.data(), app2_marker.size());
-	ptr2 += app2_marker.size();
-	memcpy(ptr2, app2_segment_size_buffer.get(), segment_length_size);
+	unsigned char *ptr2 = image + sizeof(soi) + sizeof(app0_marker) + app0_header_size;
+	memcpy(ptr2, app2_marker, sizeof(app2_marker));
+	ptr2 += sizeof(app2_marker);
+	memcpy(ptr2, app2_segment_size_buffer, segment_length_size);
 	ptr2 += segment_length_size;
-	memcpy(ptr2, icc_profile_identify.data(), icc_profile_identify.size());
-	ptr2 += icc_profile_identify.size();
+	memcpy(ptr2, icc_profile_identify, sizeof(icc_profile_identify));
+	ptr2 += sizeof(icc_profile_identify);
 	memcpy(ptr2, icc_profile, icc_profile_size);
 	ptr2 += icc_profile_size;
-	memcpy(ptr2, jpg_image_without_header.get() + soi.size() + app0_marker.size() + app0_header_size, image_size - (app0_header_size + soi.size() + app0_marker.size()));
+	memcpy(ptr2, jpg_image_without_header.get() + sizeof(soi) + sizeof(app0_marker) + app0_header_size, image_size - (app0_header_size + sizeof(soi) + sizeof(app0_marker)));
 	*output_size = expected_file_size;
 
 	return true;
